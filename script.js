69ab9314e4eb9acaf9e5ec0187c771b7eb893d5d
prudhvigana <122306094+prudhvigana@users.noreply.github.com>
2023-02-06 23:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+function addItem() {
+	var data = document.getElementById("newTodoInput").value;
 
+	if(data.length != 0){
+		var li = document.createElement("li");
+		li.innerHTML = data;
+		document.getElementById("todoList").appendChild(li);
+	}
+
+	document.getElementById("newTodoInput").value = "";
+}
